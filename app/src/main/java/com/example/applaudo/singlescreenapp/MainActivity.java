55be2f80de9ed9414502796cd7d2af13037e75af
Deryan Argueta <deryan.hndz@gmail.com>
2018-09-05 10:52:59 -0600

    second commit

---
+++ app/src/main/java/com/example/applaudo/singlescreenapp/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.applaudo.singlescreenapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+   Button dial;
+   TextView number;
+
+    public void launchDialer(String number){
+
+        String numberToDial = "tel:"+number;
+
+        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse(numberToDial));
+
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dial = findViewById(R.id.btndial); //The button
+
+        number = findViewById(R.id.vphone); //The phone number on the TextView
+
+        dial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String vnumber = number.getText().toString();
+
+                launchDialer(vnumber);
+
+            }
+        });
+
+    }
+}
